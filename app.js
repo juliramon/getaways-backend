1f79a855dd3419884f0895e1cb1885fcfb3ae633
Juli Ramon <juli@nexumlabs.com>
2020-09-10 20:12:07 +0200

    updated appjs backend

--- app.js
+++ app.js
@@ -80,4 +80,9 @@ app.use("/api", authRoutes);
 app.use("/api", contentRoutes);
 app.use("/api", require("./routes/file-upload-routes"));
 
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
+
 module.exports = app;
