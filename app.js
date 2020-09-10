be256e941f4b3aec27dfeb84c2c00ab079f26a11
Juli Ramon <juli@nexumlabs.com>
2020-09-10 20:28:09 +0200

    Updated appjs

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use("/api", contentRoutes);
 app.use("/api", require("./routes/file-upload-routes"));
 
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
