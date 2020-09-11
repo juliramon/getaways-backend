e00249c45885b5045d0e0af363a4802de6d07563
Juli Ramon <juli@nexumlabs.com>
2020-09-11 10:26:47 +0200

    new build

--- public/precache-manifest.a3adf287647fd2f48206b38fffbe5ba7.js
+++ public/precache-manifest.a3adf287647fd2f48206b38fffbe5ba7.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1009614c7d8ccefd117a61d8eac2e886",
+    "revision": "594c5fcbd30db93eaea7089da7c3fac1",
     "url": "/index.html"
   },
   {
-    "revision": "a4b9032b3219eb6a227c",
+    "revision": "209cb7fb7c9e63b2a5d2",
     "url": "/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "4f3c879d3dede0202afe",
+    "revision": "adb02eaf42f520278f88",
     "url": "/static/css/main.57071904.chunk.css"
   },
   {
-    "revision": "a4b9032b3219eb6a227c",
-    "url": "/static/js/2.6db97672.chunk.js"
+    "revision": "209cb7fb7c9e63b2a5d2",
+    "url": "/static/js/2.3b3b13c0.chunk.js"
   },
   {
     "revision": "41b26c47bc0a6b3b11aa94eb3e5ca96a",
-    "url": "/static/js/2.6db97672.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.3b3b13c0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f3c879d3dede0202afe",
-    "url": "/static/js/main.fc13d44b.chunk.js"
+    "revision": "adb02eaf42f520278f88",
+    "url": "/static/js/main.84f628ad.chunk.js"
   },
   {
     "revision": "eaacfb9e9b88ba7ef562",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ba49b7e480d77244d5eb8dbae5266b5.js"
+  "/precache-manifest.a3adf287647fd2f48206b38fffbe5ba7.js"
 );
 
 self.addEventListener('message', (event) => {
