810be86f28b87a3cf3791514d081c87366a7d6c2
Juli Ramon <juli@nexumlabs.com>
2020-09-15 19:57:56 +0200

    deploying to h

---
+++ build/precache-manifest.e0c08de33304bc48ba72e0c69994bec8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5339630e28bbcd1df8fc370b97c8221c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63324d8a353c05d7f6d4",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a61199d0a6256723708d",
+    "url": "/static/css/main.6e32876d.chunk.css"
+  },
+  {
+    "revision": "63324d8a353c05d7f6d4",
+    "url": "/static/js/2.bee17521.chunk.js"
+  },
+  {
+    "revision": "2ce68911105e4291e64ab323a88062d3",
+    "url": "/static/js/2.bee17521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61199d0a6256723708d",
+    "url": "/static/js/main.df161abc.chunk.js"
+  },
+  {
+    "revision": "eaacfb9e9b88ba7ef562",
+    "url": "/static/js/runtime-main.ed961379.js"
+  },
+  {
+    "revision": "9a9eeddb3eb9ce4f64e378a8a7b9c042",
+    "url": "/static/media/Inter-Regular.9a9eeddb.otf"
+  },
+  {
+    "revision": "1406dd01e6a812787597d6a27fe24762",
+    "url": "/static/media/MADE-Tommy-Soft-Black-PERSONAL-USE.1406dd01.woff"
+  }
+]);
